8a344bce139366ef2dd887900bca770966acb717
JayashTripathy <jayashtripathy371@gmail.com>
2023-12-14 22:54:12 +0530

    fixed sidemenu

--- src/components/Sidemenu.tsx
+++ src/components/Sidemenu.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSidemenuStore } from "@/store";
+import  {useLocation } from "react-router-dom"
 
 interface MenuItem {
   title: string;
@@ -13,22 +14,26 @@ interface SidemenuProps {
 }
 
 const Sidemenu: React.FC<SidemenuProps> = ({ menuOptions }) => {
-  // features-to-work-on: Add functionality to change active tab when we navigate to other page (for back button)
-  const activeTab = useSidemenuStore((state) => state.activeTab);
-  const setActiveTab = useSidemenuStore((state) => state.setActiveTab);
-  const navigate = useNavigate();
-  useEffect(() => {
-    setActiveTab(0);
-    const activePage = menuOptions.filter(
-      (item, index) => index === activeTab
-    )[0];
-    navigate(activePage.link);
-  }, []);
 
-  const handleClick = (index: number) => {
-    setActiveTab(index);
-    console.log(index);
-  };
+  // features-to-work-on: Add functionality to change active tab when we navigate to other page (for back button)
+  // const activeTab = useSidemenuStore((state) => state.activeTab);
+  // const setActiveTab = useSidemenuStore((state) => state.setActiveTab);
+  const location = useLocation();
+  const { pathname } = location
+
+
+  // useEffect(() => {
+  //   setActiveTab(0);
+  //   const activePage = menuOptions.filter(
+  //     (item, index) => index === activeTab
+  //   )[0];
+  //   navigate(activePage.link);
+  // }, []);
+
+  // const handleClick = (index: number) => {
+  //   setActiveTab(index);
+  //   console.log(index);
+  // };
 
   return (
     <div className=" overflow-autoDefend flex flex-col border-r-2 border-secondary gap-4 items-start w-full max-w-[250px] bg-secondary/20 py-10 h-auto">
@@ -39,9 +44,9 @@ const Sidemenu: React.FC<SidemenuProps> = ({ menuOptions }) => {
         <Link
           key={itemIndex}
           to={item.link}
-          onClick={() => handleClick(itemIndex)}
+      
           className={`${
-            activeTab === itemIndex
+            pathname === item.link
               ? "hover:bg-primary/80 bg-primary"
               : "hover:bg-secondary/50"
           } text-foreground p-4 px-6 flex gap-2 items-center w-full`}
@@ -50,6 +55,7 @@ const Sidemenu: React.FC<SidemenuProps> = ({ menuOptions }) => {
           {/* Display icon */}
           {item.title}
         </Link>
+       
       ))}
     </div>
   );
