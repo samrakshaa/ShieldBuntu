71b0b409f036c9aa19e88f3af54e70495547601d
JayashTripathy <jayashtripathy371@gmail.com>
2023-12-15 19:06:48 +0530

    fixed ssh

--- src-tauri/scripts/check/check_ssh.sh
+++ src-tauri/scripts/check/check_ssh.sh
@@ -6,8 +6,8 @@ FILE_PATH="/etc/ssh/sshd_config"
 # Check if the text "#Hardend" exists in the file
 if grep -q "#Hardend" "$FILE_PATH"; then
     # If found, echo JSON with enabled: true
-    echo '{"enabled": true}'
+    echo '{"success": true}'
 else
     # If not found, echo JSON with enabled: false
-    echo '{"enabled": false}'
+    echo '{"success": false}'
 fi
