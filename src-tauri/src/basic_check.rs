799474d23466eed6fa3b04eb475f11950ded229e
Shanks2121 <shanksy1010@gmail.com>
2023-12-15 16:19:32 +0530

    basic check rs

---
+++ src-tauri/src/basic_check.rs
@@ -0,0 +1,163 @@
+use std::process::{Stdio, Command};
+use tokio::io::AsyncWriteExt;
+use crate::get_password;
+
+use tokio::process::Command as AsyncCommand;
+
+
+
+#[tauri::command]
+pub async fn check_fail2ban() -> Result<String, String> {
+    let password = get_password().ok_or_else(|| "Password not available".to_string())?;
+    let current_dir = std::env::current_dir().map_err(|e| format!("Error getting current directory: {}", e))?;
+    let script_path = current_dir.join("scripts/check/check_fail2ban.sh");
+
+    // Run the bash script for checking fail2ban status
+    let mut child = AsyncCommand::new("sudo")
+        .arg("-S")
+        .arg("bash")
+        .arg(script_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Error spawning process: {}", e))?;
+
+    if let Some(mut stdin) = child.stdin.take() {
+        stdin.write_all(format!("{}\n", password).as_bytes()).await
+            .map_err(|e| format!("Error writing to stdin: {}", e))?;
+    }
+
+    // Await the child process completion
+    let output = child.wait_with_output().await
+        .map_err(|e| format!("Error waiting for process: {}", e))?;
+
+    // Check if the command executed successfully
+    if output.status.success() {
+        let output_str = String::from_utf8(output.stdout)
+            .map_err(|e| format!("Failed to read output: {}", e))?;
+
+        Ok(output_str)  // Return the output directly
+    } else {
+        let error_output = String::from_utf8(output.stderr)
+            .map_err(|e| format!("Failed to read error output: {}", e))?;
+        Err(format!("Error executing command: {}", error_output))
+    }
+}
+
+#[tauri::command]
+pub async fn check_calmav() -> Result<String, String> {
+    let password = get_password().ok_or_else(|| "Password not available".to_string())?;
+    let current_dir = std::env::current_dir().map_err(|e| format!("Error getting current directory: {}", e))?;
+    let script_path = current_dir.join("scripts/check/check_calmav.sh");
+
+    // Run the bash script for checking calmav status
+    let mut child = AsyncCommand::new("sudo")
+        .arg("-S")
+        .arg("bash")
+        .arg(script_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Error spawning process: {}", e))?;
+
+    if let Some(mut stdin) = child.stdin.take() {
+        stdin.write_all(format!("{}\n", password).as_bytes()).await
+            .map_err(|e| format!("Error writing to stdin: {}", e))?;
+    }
+
+    // Await the child process completion
+    let output = child.wait_with_output().await
+        .map_err(|e| format!("Error waiting for process: {}", e))?;
+
+    // Check if the command executed successfully
+    if output.status.success() {
+        let output_str = String::from_utf8(output.stdout)
+            .map_err(|e| format!("Failed to read output: {}", e))?;
+
+        Ok(output_str)  // Return the output directly
+    } else {
+        let error_output = String::from_utf8(output.stderr)
+            .map_err(|e| format!("Failed to read error output: {}", e))?;
+        Err(format!("Error executing command: {}", error_output))
+    }
+}
+
+#[tauri::command]
+pub async fn check_rkhunter() -> Result<String, String> {
+    let password = get_password().ok_or_else(|| "Password not available".to_string())?;
+    let current_dir = std::env::current_dir().map_err(|e| format!("Error getting current directory: {}", e))?;
+    let script_path = current_dir.join("scripts/check/check_rkhunter.sh");
+
+    // Run the bash script for checking rkhunter status
+    let mut child = AsyncCommand::new("sudo")
+        .arg("-S")
+        .arg("bash")
+        .arg(script_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Error spawning process: {}", e))?;
+
+    if let Some(mut stdin) = child.stdin.take() {
+        stdin.write_all(format!("{}\n", password).as_bytes()).await
+            .map_err(|e| format!("Error writing to stdin: {}", e))?;
+    }
+
+    // Await the child process completion
+    let output = child.wait_with_output().await
+        .map_err(|e| format!("Error waiting for process: {}", e))?;
+
+    // Check if the command executed successfully
+    if output.status.success() {
+        let output_str = String::from_utf8(output.stdout)
+            .map_err(|e| format!("Failed to read output: {}", e))?;
+
+        Ok(output_str)  // Return the output directly
+    } else {
+        let error_output = String::from_utf8(output.stderr)
+            .map_err(|e| format!("Failed to read error output: {}", e))?;
+        Err(format!("Error executing command: {}", error_output))
+    }
+}
+
+#[tauri::command]
+pub async fn check_unused_package() -> Result<String, String> {
+    let password = get_password().ok_or_else(|| "Password not available".to_string())?;
+    let current_dir = std::env::current_dir().map_err(|e| format!("Error getting current directory: {}", e))?;
+    let script_path = current_dir.join("scripts/check/check_unused_package.sh");
+
+    // Run the bash script for checking fail2ban status
+    let mut child = AsyncCommand::new("sudo")
+        .arg("-S")
+        .arg("bash")
+        .arg(script_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Error spawning process: {}", e))?;
+
+    if let Some(mut stdin) = child.stdin.take() {
+        stdin.write_all(format!("{}\n", password).as_bytes()).await
+            .map_err(|e| format!("Error writing to stdin: {}", e))?;
+    }
+
+    // Await the child process completion
+    let output = child.wait_with_output().await
+        .map_err(|e| format!("Error waiting for process: {}", e))?;
+
+    // Check if the command executed successfully
+    if output.status.success() {
+        let output_str = String::from_utf8(output.stdout)
+            .map_err(|e| format!("Failed to read output: {}", e))?;
+
+        Ok(output_str)  // Return the output directly
+    } else {
+        let error_output = String::from_utf8(output.stderr)
+            .map_err(|e| format!("Failed to read error output: {}", e))?;
+        Err(format!("Error executing command: {}", error_output))
+    }
+}
