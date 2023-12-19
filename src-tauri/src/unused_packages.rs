20c6dae021f32972a81b9e231d2c0251e62135b6
Shashwat79802 <shashwatg79802@gmail.com>
2023-12-19 22:44:33 +0530

    Refactor unused_packages.rs

--- src-tauri/src/unused_packages.rs
+++ src-tauri/src/unused_packages.rs
@@ -1,74 +1,59 @@
-use std::fs::OpenOptions;
-use std::io::{Read, Write};
-use std::process::Stdio;
-use tokio::process::Command as AsyncCommand;
-use tokio::io::AsyncWriteExt;
+use std::io::Write;
+use std::process::{Command, Stdio};
+use std::fs::{OpenOptions, self};
+use std::path::Path;
+use std::env;
 use chrono::Utc;
+use serde_json::json;
+use tauri::AppHandle;
+// use tokio::io::AsyncWriteExt;
+
 use crate::get_password;
 
 #[tauri::command]
-pub async fn remove_unused_packages() -> Result<String, String> {
-    let sudo_password = get_password();
+pub async fn remove_unused_packages(handle: AppHandle) -> Result<String, String> {
+    let password = get_password().ok_or_else(|| "Password not available".to_string())?;
 
-    if let Some(password) = sudo_password {
-        let current_dir = std::env::current_dir().map_err(|e| format!("Error getting current directory: {}", e))?;
-        let script_path = current_dir.join("scripts/apply/unused_package_remover.sh");
-        let log_file_path = current_dir.join("logs/unused_package_removal_log.txt");
+    let log_directory = match env::var("HOME") {
+        Ok(home) => format!("{}/.samrakshak_logs", home),
+        Err(_) => return Err("Could not retrieve user's home directory".to_string()),
+    };
 
-        // Open or create the log file for appending
-        let mut file = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open(&log_file_path)
-            .map_err(|e| format!("Error opening log file: {}", e))?;
+    fs::create_dir_all(&log_directory)
+        .map_err(|e| format!("Error creating directory: {}", e))?;
 
-        // Write the current date and time to the log file
-        let datetime = Utc::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
-        file.write_all(format!("\n\n{}\n\n", datetime).as_bytes())
-            .map_err(|e| format!("Error writing to log file: {}", e))?;
+    let script_path = handle
+        .path_resolver()
+        .resolve_resource("scripts/apply/unused_package_remover.sh")
+        .expect("failed to resolve resource");
 
-        // Run the bash script for removing unused packages
-        let mut child = AsyncCommand::new("sudo")
-            .arg("-S")
-            .arg("bash")
-            .arg(&script_path)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-            .map_err(|e| format!("Error spawning process: {}", e))?;
+    let log_file_path = Path::new(&log_directory).join("unused_package_removal_log.txt");
 
-        if let Some(mut stdin) = child.stdin.take() {
-            stdin.write_all(password.as_bytes()).await.map_err(|e| format!("Error writing to stdin: {}", e))?;
-        }
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&log_file_path)
+        .map_err(|e| format!("Error opening log file: {}", e))?;
 
-        // Capture the output
-        let output = child.wait_with_output().await.map_err(|e| format!("Error waiting for process: {}", e))?;
-        
-        // Write the output to the log file
-        file.write_all(&output.stdout)
-            .and_then(|_| file.write_all(&output.stderr))
-            .map_err(|e| format!("Error writing to log file: {}", e))?;
+    let datetime = Utc::now().format("[%Y-%m-%d %H:%M:%S]").to_string();
+    file.write_all(format!("\n\n{}\n\n", datetime).as_bytes())
+        .map_err(|e| format!("Error writing to log file: {}", e))?;
 
-        // Read the entire log file to include in the response
-        let mut file = OpenOptions::new()
-            .read(true)
-            .open(&log_file_path)
-            .map_err(|e| format!("Error opening log file: {}", e))?;
+    let mut child = Command::new("bash")
+        .arg(script_path.to_str().unwrap())
+        .arg(&password)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Error spawning process: {}", e))?;
 
-        let mut log_contents = String::new();
-        file.read_to_string(&mut log_contents)
-            .map_err(|e| format!("Error reading log file: {}", e))?;
+    let output = child.wait_with_output()
+        .map_err(|e| format!("Error waiting for process: {}", e))?;
 
-        // Construct the JSON-like return value
-        let result = if output.status.success() {
-            format!(r#"{{"success": true, "logs": "{}"}}"#, log_contents)
-        } else {
-            format!(r#"{{"success": false, "logs": "{}"}}"#, log_contents)
-        };
+    file.write_all(&output.stdout)
+        .map_err(|e| format!("Error writing to log file: {}", e))?;
 
-        Ok(result)
-    } else {
-        Err("Sudo password not available".to_string())
-    }
+    let result = json!({ "logs": String::from_utf8_lossy(&output.stdout).trim() }).to_string();
+    Ok(result)
 }
